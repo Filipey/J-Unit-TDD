281502c24d4218cbdcb5df59119847c5569e0dd4
Filipe Moura <filipeasm18@gmail.com>
2022-07-14 17:08:28 -0300

    refactor to junit 5 and mockito

--- src/test/java/com/filipey/CalculatorTest.java
+++ src/test/java/com/filipey/CalculatorTest.java
@@ -3,18 +3,26 @@ package com.filipey;
 
 import com.filipey.models.Calculator;
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class CalculatorTest {
+
+    @Mock
     Calculator calculator;
 
-    @Before
-    public void setUp() {
+    @BeforeEach
+    public  void setUp() {
         calculator = new Calculator();
     }
 
     @Test
+    @DisplayName("Should sum two numbers correctly")
     public void shouldSumTwoNumbers() {
         // cenario
         int num = 10, num2 = 5;
@@ -27,15 +35,18 @@ public class CalculatorTest {
 
     }
 
-    @Test(expected = RuntimeException.class)
+    @Test
+    @DisplayName("Should dont sum negative numbers")
     public void shouldDontSumNegativeNumbers() {
         int num = -10, num2 = 5;
 
-        int result = calculator.sum(num, num2);
+        org.junit.jupiter.api.Assertions
+                .assertThrows(RuntimeException.class, () -> calculator.sum(num, num2));
 
     }
 
     @Test
+    @DisplayName("Should subtract two numbers correctly")
     public void shouldSubTwoNumbers() {
         int num = 20, num2 = 15;
 
@@ -45,7 +56,8 @@ public class CalculatorTest {
     }
 
     @Test
-    public void shouldMultiplyTwoNumbers() {
+    @DisplayName("Should multiply two numbers correctly")
+    public void shouldMultTwoNumbers() {
         int num = 10, num2 = 2;
 
         int result = calculator.mult(num, num2);
@@ -54,7 +66,8 @@ public class CalculatorTest {
     }
 
     @Test
-    public void shouldDivideTwoNumbers() {
+    @DisplayName("Should divide two numbers correctly")
+    public void shouldDivTwoNumbers() {
         int num = 10, num2 = 2;
 
         float result = calculator.div(num, num2);
@@ -62,11 +75,13 @@ public class CalculatorTest {
         Assertions.assertThat(result).isEqualTo(5);
     }
 
-    @Test(expected = ArithmeticException.class)
+    @Test
+    @DisplayName("Should dont divide a number by 0")
     public void shouldDontDivideByZero() {
         int num = 10, num2 = 0;
 
-        float result = calculator.div(num, num2);
+        org.junit.jupiter.api.Assertions
+                .assertThrows(ArithmeticException.class, () -> calculator.div(num, num2));
 
     }
 }
